f81ae7889077426811f9f20b14c98c74f68026c0
Alex Cevallos <cevalloa@gmail.com>
2015-04-21 00:26:21 -0400

    Added tests & validations for user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,21 @@ class User < ActiveRecord::Base
 
 	#validates the name field to not be empty
 	validates :name, presence: true
+	#validates name field to max length of 50
+	validates :name, presence: true, length: {maximum: 50}
 
 	#validates the email field to not be empty
 	validates :email, presence: true
+	#validates email field to max 255
+	validates :email, presence: true, length: {maximum: 255}
+
+	#for regular expression
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+
+	#validates correct email format
+	validates :email, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false }
+
+	#validates emails should be unique
+
 end
