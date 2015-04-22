150955f264bed722bca398d3f97505224ef34d40
Alex Cevallos <cevalloa@gmail.com>
2015-04-21 23:09:34 -0400

    Added validations & tests for unique entries

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+	before_save {self.email = email.downcase }
+
 	#validates the name field to not be empty
 	validates :name, presence: true
 	#validates name field to max length of 50
@@ -17,6 +19,5 @@ class User < ActiveRecord::Base
 	#validates correct email format
 	validates :email, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false }
 
-	#validates emails should be unique
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418162500) do
+ActiveRecord::Schema.define(version: 20150421044639) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
