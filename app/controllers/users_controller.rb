5f69d359c4a9bab83af8ca9d13b0a63f4b21bae9
Alex Cevallos <cevalloa@gmail.com>
2015-05-15 14:00:00 +0100

    Added logic to handle signup errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
 
   def show
-
   	@user = User.find(params[:id])
+  end
 
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		#successful save
+  	else
+  		render 'new'
+  	end
   end
 end
